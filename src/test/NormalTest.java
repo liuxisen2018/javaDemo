c3310add484e60ba10e1db8ab3bd3f93b1aac995
刘希森 <liuxisen@idcos.com>
2019-07-07 14:24:09 +0800

    just commit some code

--- src/test/NormalTest.java
+++ src/test/NormalTest.java
@@ -1,17 +1,15 @@
 package test;
 
-import java.util.Random;
-
 /**
  * @author: liuxs
- * @description:
  * @date: Created in 11:05 AM  2019/2/26
  */
 public class NormalTest {
    public static void main(String[] args){
-       Random random = new Random();
-       int b = random.nextInt(1);
-       int a = b % 2;
-       System.out.println(b);
+      String str1 =  new String("sen");
+      String str2 =  new String("sen");
+
+      System.out.println(str1.hashCode());
+      System.out.println(str2.hashCode());
    }
 }
